4dc9abaabaef00f93660b393d0ebc0c79edffc69
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2019-10-11 07:58:33 +0200

    Updated example

--- example-06/App_Plugins/Example06/controller01.js
+++ example-06/App_Plugins/Example06/controller01.js
@@ -5,6 +5,10 @@
     var vm = this;
 
     $timeout(function() {
+      vm.variant = _.find($scope.content.variants, function(v) {
+        return v.active;
+      });
+      vm.variant.apps = _.shuffle(vm.variant.apps);
       $scope.model.view = "/App_Plugins/Example06/view.html";
     }, 1000);
   }
